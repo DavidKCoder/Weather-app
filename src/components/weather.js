06a37d5bd1254e7ebb43f9873ee4300ff6ce6885
DavidKizinger <karapetyand91@gmail.com>
2020-07-21 13:26:20 +0400

    new file

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
-const Weather = props => (
-    <div className="infoWeath">
-        {props.city &&
-            <div>
-                <p>Location: {props.city}, {props.country}</p>
-                <p>Temp: {props.isLoading ? `${props.tempC} C°` : `${props.tempF} F°`} <i class="fa fa-thermometer-three-quarters"></i></p>
-                <p>Pressure: {props.pressure}</p>
-                <p>Sunset: {props.sunset}</p>
-                <button onClick={props.degree} className="btn btn-warning btn-sm degrees">{props.isLoading ? 'F°' : 'C°'}</button>
-            </div>
-        }
-        <p className="error">{props.error} </p>
-    </div>
-);
+export const Weather = (props) => {
+    return (
+        <>
+            {props.city &&
+                <>
+                    <main>
+                        <div className='weather-main'>
+                            <img
+                                src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt='weather icon'
+                                className='weather-icon' />
+                            <div>
+                                <h2>{props.city}</h2>
+                            </div>
+                        </div>
+                        <div className='temp-main'>
+                            <h1 className='temperature'>{props.isLoaded ? `${props.temp} °C` : `${props.tempF} °F`}</h1>
+                            <button className="btn-degree" onClick={props.degree}><ion-icon name="sync-outline"></ion-icon> <h3>{props.isLoaded ? '°F' : `°C`}</h3> </button>
+                            <div className='hi-lo'>
+                                <h5>H {props.isLoaded ? `${props.temp_max_C} °C` : `${props.temp_max_F} °F`}°</h5>
+                                <h5>L {props.isLoaded ? `${props.temp_min_C} °C` : `${props.temp_min_F} °F`}°</h5>
+                            </div>
+                        </div>
+                    </main>
+                </>
+            }
+        </>
+    )
 
-export default Weather;
+}
