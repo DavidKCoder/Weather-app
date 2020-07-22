7d364504d2af25338bbde3c8acb18579ab2e473c
DavidKizinger <karapetyand91@gmail.com>
2020-07-22 20:24:31 +0400

    updated last base form

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -3,7 +3,7 @@ import { DegreesToDirection, Month, Weekday, Day } from '../helpers/utils'
 
 
 export const WeatherData = ({ data, isLoaded, degree }) => {
-    const { name, country, tempC, tempF, description,icon, temp_min_C,temp_min_F,temp_max_C,temp_max_F, feels_like_C,feels_like_F, speed, deg, humidity } = data;
+    const { name, country, tempC, tempF, description, icon, temp_min_C, temp_min_F, temp_max_C, temp_max_F, feels_like_C, feels_like_F, speed, deg, humidity } = data;
 
     return (
         <>
@@ -21,7 +21,7 @@ export const WeatherData = ({ data, isLoaded, degree }) => {
                     </div>
                 </div>
                 <div className='temp-main'>
-                    <h5>Feels like {isLoaded ? `${feels_like_C} °C` : `${feels_like_F} °F`} °</h5>
+                    <h5>Feels like {isLoaded ? `${feels_like_C} °C` : `${feels_like_F} °F`}</h5>
                     <h1 className='temperature'>{isLoaded ? `${tempC} °C` : `${tempF} °F`}</h1>
                     <button className="btn-degree" onClick={degree}><ion-icon name="sync-outline"></ion-icon> <h3>{isLoaded ? '°F' : `°C`}</h3> </button>
                     <div className='hi-lo'>
