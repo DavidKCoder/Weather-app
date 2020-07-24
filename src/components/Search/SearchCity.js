ba664b5642c8dd83176cf4a923ebae7413f58858
DavidKizinger <karapetyand91@gmail.com>
2020-07-24 12:30:53 +0400

    new folder

---
+++ src/components/Search/SearchCity.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const SearchCity = ({ submit, value, change, showResult }) => {
+    return (
+        <>
+            <form className="form" showResult={showResult} onSubmit={submit}>
+                <input type="text" value={value} placeholder="Enter city" onChange={change} />
+            </form>
+        </>
+    );
+};
+
+// SearchCity.propTypes = {
+//     submit: PropTypes.func.isRequired,
+//     value: PropTypes.string.isRequired,
+//     change: PropTypes.func.isRequired,
+//     showResult: PropTypes.bool.isRequired,
+// };
+
+export default SearchCity;
