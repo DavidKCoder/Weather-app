40f671a615f3627129e76ea85e6595a7820c20a7
DavidKizinger <karapetyand91@gmail.com>
2020-08-03 11:46:25 +0400

    rm

--- src/components/City/CityNotFound/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NotFound = () => {
-  return (
-    <div className="notFound">Sorry, the specified city was not found..</div>
-  );
-};
-
-export default NotFound;
