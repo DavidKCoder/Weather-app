3e0d6ca01a853bee03a311ff5202f60dfbc9a5ef
DavidKizinger <karapetyand91@gmail.com>
2020-08-03 11:41:29 +0400

    fixed

---
+++ src/components/City/NotFound/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className="notFound">Sorry, the specified city was not found..</div>
+  );
+};
+
+export default NotFound;
---
+++ src/components/City/Search/Search.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SearchCity = ({ submit, value, change }) => {
+    return (
+        <form className="form" onSubmit={submit}>
+            <input
+                type="text"
+                value={value}
+                placeholder="Enter city"
+                onChange={change} />
+        </form>
+    );
+};
+
+SearchCity.propTypes = {
+    submit: PropTypes.func.isRequired,
+    value: PropTypes.string.isRequired,
+    change: PropTypes.func.isRequired,
+};
+
+export default SearchCity;
